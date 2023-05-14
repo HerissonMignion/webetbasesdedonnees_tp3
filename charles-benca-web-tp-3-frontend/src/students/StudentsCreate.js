c54296d9a1df338dc15dd880ae57980b117e378d
Charles Benca <zzazzbug@gmail.com>
2023-05-14 14:17:15 -0400

    terminé la page de création des étudiants

--- charles-benca-web-tp-3-frontend/src/students/StudentsCreate.js
+++ charles-benca-web-tp-3-frontend/src/students/StudentsCreate.js
@@ -1,16 +1,162 @@
 import React from "react";
 
+import { useState } from "react";
+import config from "../config";
 
+import { NavLink } from "react-router-dom";
 import StudentsNavBar from "./components/StudentsNavBar";
 
 
 function StudentsCreate() {
+    const [fullName, setFullName] = useState("");
+    const [daNumber, setDaNumber] = useState("");
+    const [email, setEmail] = useState("");
+    const [profile, setProfile] = useState("programming");
 
+    const [created, setCreated] = useState(false);
+    const [creationError, setCreationError] = useState(false); // indique s'il y a eu une erreur pendant la création
+    const [creationErrorMessage, setCreationErrorMessage] = useState(""); 
+
+    function buttonCreateStudent() {
+        const student = {
+            fullName,
+            daNumber,
+            email,
+            profile
+        };
+
+        // console.log(student);
+
+        (async () => {
+            const response = await fetch(config.backend + "/api/students/create", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(student)
+            });
+
+            const responseJson = await response.json();
+            // console.log(responseJson);
+
+            //si y'a un étudiant, alors c'est un succès
+            if (responseJson.student) {
+                setCreated(true);
+                setCreationError(false);
+                setCreationErrorMessage("");
+            }
+            else {
+                setCreated(false);
+                setCreationError(true);
+                setCreationErrorMessage(responseJson.message);
+            }
+
+        })();
+    }
 
     return (
         <>
             <StudentsNavBar />
             Créer un nouveau étudiant
+            {!created
+            && 
+                <div>
+                    <label>
+                        Nom complet : 
+                        <input
+                            type="text"
+                            onChange={(e) => {
+                                setFullName(e.target.value);
+                            }}
+                            value={fullName}
+                        />
+                    </label>
+                    <br></br>
+                    <label>
+                        Numéro de DA :
+                        <input
+                            type="text"
+                            onChange={(e) => {
+                                setDaNumber(e.target.value);
+                            }}
+                        />
+                    </label>
+                    <br></br>
+                    <label>
+                        Courriel :
+                        <input
+                            type="text"
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                            }}
+                        />
+                    </label>
+                    <br></br>
+                    <label>
+                        Profile :
+                        <select
+                            onChange={(e) => {
+                                setProfile(e.target.value);
+                            }}
+                            value={profile}
+                        >
+                            <option
+                                key="0"
+                                value="programming"
+                            >
+                                Programmation
+                            </option>
+                            <option
+                                key="1"
+                                value="networking"
+                            >
+                                Réseautique
+                            </option>
+                        </select>
+                    </label>
+                    <br></br>
+                    <button
+                        onClick={buttonCreateStudent}
+                    >
+                        Créer l'étudiant
+                    </button>
+                </div>
+            }
+            {
+                created
+                &&
+                    <div>
+                        L'étudiant a été créé avec succès
+                        <br></br>
+                        <NavLink to="/students/home">
+                            <button>Retourner à l'accueil des étudiants</button>
+                        </NavLink>
+                        <br></br>
+                        <button
+                            onClick={(e) => {
+                                setFullName("");
+                                setDaNumber("");
+                                setEmail("");
+                                setProfile("programming");
+                                setCreated(false);
+                                setCreationError(false);
+                                setCreationErrorMessage("");
+                            }}
+                        >
+                            Créer un nouveau étudiant
+                        </button>
+                    </div>
+            }
+            {
+                creationError
+                &&
+                    <div>
+                        Une erreur s'est produite pendant la création de l'étudiant :
+                        <p>
+                            {creationErrorMessage}
+                        </p>
+                    </div>
+            }
         </>
     );
 
